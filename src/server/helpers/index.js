a77556f770b9f52bdcb05a34898448cf1111b53e
abidex4yemi <abidex4yemi@gmail.com>
2019-07-15 14:13:38 +0100

    Set up joi validation

--- src/server/helpers/index.js
+++ src/server/helpers/index.js
@@ -1,3 +1,4 @@
 // Export all utility files
 export { createError, BAD_REQUEST, CONFLICT, NOT_FOUND, GENERIC_ERROR } from './error';
 export { OK, CREATED, responseDataFormat } from './success';
+export { joiValidate } from './joiValidate';
