ba690396a901799c5d4656076c3e79f52e57cdbb
abidex4yemi <abidex4yemi@gmail.com>
2019-07-15 12:16:22 +0100

    Complete database(knex and sqlite3) setup

---
+++ src/config/database.js
@@ -0,0 +1,15 @@
+'use strict';
+
+// Get node env if exist or default to development
+const env = process.env.NODE_ENV || 'development';
+
+/**
+ * Module dependencies
+ */
+const knexModule = require('knex');
+const knexConfig = require('../knexfile');
+
+// Initialized knex based on environment variables
+const knex = knexModule(knexConfig[env]);
+
+module.exports = knex;
